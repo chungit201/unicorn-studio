f131dab5789bda400b28b787410a9ae5c271a1d1
chungit201 <vuongvanchung30092001@gmail.com>
2023-01-18 13:23:41 +0700

    update ui

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <title>Unicron Studio</title>
+        <link rel="icon" href="/icon.png"/>
         <link
           rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
           integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
