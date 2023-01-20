bb2ac61ee1c1f04158fa36605cc38c43ece05586
chungit201 <vuongvanchung30092001@gmail.com>
2023-01-21 02:23:01 +0700

    responsive

--- common/compoments/Slider.tsx
+++ common/compoments/Slider.tsx
@@ -3,94 +3,73 @@ import slider1 from "../assets/images/image_review1.png";
 import slider2 from "../assets/images/image_review2.png";
 import slider3 from "../assets/images/image_review3.png"
 import "react-alice-carousel/lib/alice-carousel.css";
-import AliceCarousel from 'react-alice-carousel';
 import Image from "next/image";
 import frame_inactive from "@/common/assets/images/vien.png";
+import frame_active from "@/common/assets/images/vien2.png";
+// @ts-ignore
+import Whirligig from 'react-whirligig'
 
-const items = [
-  <div key={1} style={{position:"relative"}}>
-    <Image  className="item w-100" alt={""} style={{height: "auto"}} src={slider1}/>
-    <div style={{position: "absolute", top: "-6px"}}>
-      <Image style={{width: "100%"}} src={frame_inactive} alt={""}/>
-    </div>
-  </div>,
-  <div key={1} style={{position:"relative"}}>
-    <Image  className="item w-100" alt={""} style={{height: "auto"}} src={slider2}/>
-    <div style={{position: "absolute", top: "-6px"}}>
-      <Image style={{width: "100%"}} src={frame_inactive} alt={""}/>
-    </div>
-  </div>,
-  <div key={1} style={{position:"relative"}}>
-    <Image  className="item w-100" alt={""} style={{height: "auto"}} src={slider3}/>
-    <div style={{position: "absolute", top: "-6px"}}>
-      <Image style={{width: "100%"}} src={frame_inactive} alt={""}/>
-    </div>
-  </div>,
-  <div key={1} style={{position:"relative"}}>
-    <Image  className="item w-100" alt={""} style={{height: "auto"}} src={slider1}/>
-    <div style={{position: "absolute", top: "-6px"}}>
-      <Image style={{width: "100%"}} src={frame_inactive} alt={""}/>
-    </div>
-  </div>,
-  <div key={1} style={{position:"relative"}}>
-    <Image  className="item w-100" alt={""} style={{height: "auto"}} src={slider2}/>
-    <div style={{position: "absolute", top: "-6px"}}>
-      <Image style={{width: "100%"}} src={frame_inactive} alt={""}/>
-    </div>
-  </div>,
-  <div key={1} style={{position:"relative"}}>
-    <Image  className="item w-100" alt={""} style={{height: "auto"}} src={slider3}/>
-    <div style={{position: "absolute", top: "-6px"}}>
-      <Image style={{width: "100%"}} src={frame_inactive} alt={""}/>
-    </div>
-  </div>,
-];
-const thumbItems = (items: any, [setThumbIndex, setThumbAnimation]: any) => {
-  return items.map((item: any, i: number) => (
-    <div key={i} className="thumb" onClick={() => (setThumbIndex(i), setThumbAnimation(true))}>
-      {item}
-    </div>
-  ));
-};
 
 const Slider: React.FunctionComponent = () => {
-  const [thumbIndex, setThumbIndex] = useState(0);
-  const [thumbs] = useState(thumbItems(items, [setThumbIndex]));
-  const responsive = {
-    0: {items: 1},
-    568: {items: 2},
-    1024: {items: 3},
-  };
-
-
-  const slideNext = () => {
-    if (thumbIndex < thumbs.length - 1) {
-      setThumbIndex(thumbIndex + 1);
-    }
-  };
-  console.log(thumbIndex)
-  const slidePrev = () => {
-    if (thumbIndex > 0) {
-      setThumbIndex(thumbIndex - 1);
-    }
-  };
-
-  const syncThumbs = (e: any) => {
-    setThumbIndex(e.item);
-  };
+  let whirligig: any
+  const next = () => whirligig.next()
+  const prev = () => whirligig.prev()
   return (
-    <div style={{position:"relative"}}>
-      <AliceCarousel
-        activeIndex={thumbIndex}
-        items={items}
-        responsive={responsive}
-        disableDotsControls={true}
-        disableButtonsControls={true}
-        onSlideChanged={syncThumbs}
-      />
-      <i className="fa-solid fa-caret-left btn-prev" onClick={slidePrev} style={{fontSize: "38px", color: "#FFFFFF"}}/>
-      <i className="fa-solid fa-caret-right btn-next" onClick={slideNext} style={{fontSize: "38px", color: "#FFFFFF"}}/>
-
+    <div style={{position: "relative"}}>
+      <i className="fa-solid fa-caret-left btn-prev" onClick={prev}
+         style={{fontSize: "38px", color: "#FFFFFF", cursor: "pointer", zIndex: 10}}/>
+      <Whirligig
+        style={{overflow: "hidden"}}
+        visibleSlides={3}
+        className={"slider-whir"}
+        gutter="2em"
+        ref={(_whirligigInstance: any) => {
+          whirligig = _whirligigInstance
+        }}
+      >
+        <div style={{position: "relative"}}>
+          <Image className={"w-100"} style={{height: "auto"}} alt={""} src={slider1}/>
+          <Image className={"frame_inactive"} src={frame_inactive} alt={""}/>
+          <div className={"p-3 slider-desc"} style={{position:'absolute',bottom:0}}>
+            <h2 className={"title-title"}>Startverse</h2>
+            <p className={"mb-0"}>STARTVERSE is a metaverse for everyone, who wants to build their own startups. Players join into the
+              world to build their dream life, dream companies, meet their dream couple, hire friends to work for them,
+              and earn real money.</p>
+          </div>
+        </div>
+        <div style={{position: "relative"}}>
+          <Image className={"w-100"} style={{height: "auto"}} alt={""} src={slider2}/>
+          <Image className={"frame_inactive"} src={frame_active} alt={""}/>
+          <div className={"p-3 slider-desc"} style={{position:'absolute',bottom:0}}>
+            <h2 className={"title-title"}>Dragon Warrior</h2>
+            <p className={"mb-0"}>STARTVERSE is a metaverse for everyone, who wants to build their own startups. Players join into the
+              world to build their dream life, dream companies, meet their dream couple, hire friends to work for them,
+              and earn real money.</p>
+          </div>
+        </div>
+        <div style={{position: "relative"}}>
+          <Image className={"w-100"} style={{height: "auto"}} alt={""} src={slider3}/>
+          <Image className={"frame_inactive"} src={frame_inactive} alt={""}/>
+          <div className={"p-3 slider-desc"} style={{position:'absolute',bottom:0}}>
+            <h2 className={"title-title"}>Fit the Fat</h2>
+            <p className={"mb-0"}>STARTVERSE is a metaverse for everyone, who wants to build their own startups. Players join into the
+              world to build their dream life, dream companies, meet their dream couple, hire friends to work for them,
+              and earn real money.</p>
+          </div>
+        </div>
+        <div style={{position: "relative"}}>
+          <Image className={"w-100"} style={{height: "auto"}} alt={""} src={slider3}/>
+          <Image className={"frame_inactive"} src={frame_inactive} alt={""}/>
+          <div className={"p-3 slider-desc"} style={{position:'absolute',bottom:0}}>
+            <h2 className={"title-title"}>Fit the Fat</h2>
+            <p className={"mb-0"}>STARTVERSE is a metaverse for everyone, who wants to build their own startups. Players join into the
+              world to build their dream life, dream companies, meet their dream couple, hire friends to work for them,
+              and earn real money.</p>
+          </div>
+        </div>
+      </Whirligig>
+      <i className="fa-solid fa-caret-right btn-next" onClick={next}
+         style={{fontSize: "38px", color: "#FFFFFF", cursor: "pointer", zIndex: 100}}/>
     </div>
   )
 }
